7c4b42e53dcdf0fd67ab9137ad48f130cb7625e0
Olga Stepanova <triazed@gmail.com>
2026-09-04 12:39:24 +0500

    Migrate auth tests and add negative cases

--- test_data/auth_credentials.py
+++ test_data/auth_credentials.py
@@ -4,3 +4,20 @@ class AuthCredentials:
         "username": "admin",
         "password": "password123"
     }
+
+    invalid_admin_credentials = [
+        {
+            "username": "adm",
+            "password": "password123"
+        },
+        {
+            "username": "admin",
+            "password": "pass"
+        },
+        {
+            "username": "admin",
+        },
+        {
+            "password": "password123"
+        },
+    ]
--- tests/test_auth.py
+++ tests/test_auth.py
@@ -1,15 +1,28 @@
-import requests
-from config import urls
-from test_data import auth_credentials
+from test_data.auth_credentials import AuthCredentials
+import pytest
 
 
 class TestAuth:
 
-    def test_create_token_returns_token(self):
-        admin_credentials = auth_credentials.admin_credentials
-        response = requests.post(urls.AUTH_URL, json=admin_credentials)
-        data = response.json()
+    def test_create_token_with_valid_credentials_returns_token(self, auth_client):
+        response = auth_client.create_token(AuthCredentials.admin_credentials)
         assert response.status_code == 200
+
+        data = response.json()
+        assert isinstance(data, dict)
+        assert data
         assert "token" in data
         assert isinstance(data["token"], str)
         assert data["token"]
+
+    @pytest.mark.parametrize("invalid_credentials", AuthCredentials.invalid_admin_credentials)
+    def test_create_token_invalid_credentials_returns_error(self, auth_client, invalid_credentials):
+        response = auth_client.create_token(invalid_credentials)
+        assert response.status_code == 200
+
+        data = response.json()
+        assert isinstance(data, dict)
+        assert data
+        assert data["reason"] == "Bad credentials"
+        assert "token" not in data
+
