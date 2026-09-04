807546f2e876c4b918ec1334cb91d356ff2bad8c
Olga Stepanova <triazed@gmail.com>
2026-09-04 12:45:15 +0500

    Migrate ping test

--- tests/test_ping.py
+++ tests/test_ping.py
@@ -1,9 +1,8 @@
-import requests
-from config import urls
+from config.urls import Urls
 
 
 class TestPing:
 
-    def test_ping_returns_created_status(self):
-        response = requests.get(urls.PING_URL)
+    def test_ping_returns_created_status(self, unauthorized_session):
+        response = unauthorized_session.get(f"{Urls.BASE_URL}{Urls.PING_URL}")
         assert response.status_code == 201
